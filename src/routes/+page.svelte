71bed9512dc563a228a4f76d22b665b162f367bf
NickCoder123 <nikunjmehra935@gmail.com>
2023-07-12 20:47:01 +0530

    Setup Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<style>
+  h1, p {
+    @apply text-6xl text-green-600 text-center font-extrabold;
+  }
+</style>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
